a832f4439d4a1f11a562966f07b37c1aa2cf57f2
preeti.a.saxena <preeti.a.saxena@P5LAP-7230295.Home>
2019-04-19 18:17:51 +0530

    first commit

---
+++ GroovyProject/src/com/groovy/pkg/FirstGroovyClass.groovy
@@ -0,0 +1,9 @@
+package com.groovy.pkg
+
+class FirstGroovyClass {
+
+	static void main(args) {
+		// TODO Auto-generated method stub
+		println("Hello World")
+	}
+}
---
+++ GroovyProject/src/com/groovy/pkg/StringGroovy.groovy
@@ -0,0 +1,7 @@
+package com.groovy.pkg
+
+println("Hello World")
+def name = "Raghav"
+println name.length()
+println name[2]
+println name.indexOf('g')
